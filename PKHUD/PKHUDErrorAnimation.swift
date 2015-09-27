551fb3bc8b34551047405e3110c2a197acf66d74
Philip Kluz <philip.kluz@originate.com>
2015-09-27 19:46:19 +0200

    Refactor.

--- PKHUD/PKHUDErrorAnimation.swift
+++ PKHUD/PKHUDErrorAnimation.swift
@@ -11,7 +11,10 @@ import UIKit
 /// PKHUDErrorAnimation provides an animated error (cross) view.
 public class PKHUDErrorAnimation: PKHUDSquareBaseView, PKHUDAnimating {
     
-    var dashOneLayer: CAShapeLayer = {
+    var dashOneLayer = PKHUDErrorAnimation.generateDashLayer()
+    var dashTwoLayer = PKHUDErrorAnimation.generateDashLayer()
+    
+    class func generateDashLayer() -> CAShapeLayer {
         let dash = CAShapeLayer()
         dash.frame = CGRectMake(0.0, 0.0, 88.0, 88.0)
         dash.path = {
@@ -26,28 +29,8 @@ public class PKHUDErrorAnimation: PKHUDSquareBaseView, PKHUDAnimating {
         dash.strokeColor = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.0).CGColor
         dash.lineWidth   = 6
         dash.fillMode = kCAFillModeForwards;
-        
         return dash
-    }()
-    
-    var dashTwoLayer: CAShapeLayer = {
-        let dash = CAShapeLayer()
-        dash.frame = CGRectMake(0.0, 0.0, 88.0, 88.0)
-        dash.path = {
-            let path = UIBezierPath()
-            path.moveToPoint(CGPointMake(0.0, 44.0))
-            path.addLineToPoint(CGPointMake(88.0, 44.0))
-            return path.CGPath
-            }()
-        dash.lineCap     = kCALineCapRound
-        dash.lineJoin    = kCALineJoinRound
-        dash.fillColor   = nil
-        dash.strokeColor = UIColor(red: 0.15, green: 0.15, blue: 0.15, alpha: 1.0).CGColor
-        dash.lineWidth   = 6
-        dash.fillMode = kCAFillModeForwards;
-        
-        return dash
-    }()
+    }
 
     public override init() {
         super.init()
@@ -66,26 +49,21 @@ public class PKHUDErrorAnimation: PKHUDSquareBaseView, PKHUDAnimating {
         dashTwoLayer.position = layer.position
     }
     
-    //MARK: - Animation Setup
+    func springyRotationAnimation(angle: CGFloat) -> CASpringAnimation {
+        let animation = CASpringAnimation(keyPath:"transform.rotation.z")
+        animation.fromValue = 0.0
+        animation.toValue = angle * CGFloat(M_PI / 180.0)
+        animation.duration = 1.0
+        animation.damping = 1.5
+        animation.mass = 0.22
+        animation.initialVelocity = 0.5
+        animation.timingFunction = CAMediaTimingFunction(name:kCAMediaTimingFunctionEaseInEaseOut)
+        return animation
+    }
     
     func startAnimation() {
-        let dashOneAnimation = CASpringAnimation(keyPath:"transform.rotation.z")
-        dashOneAnimation.fromValue = 0.0
-        dashOneAnimation.toValue = -45 * CGFloat(M_PI/180)
-        dashOneAnimation.duration = 1.0
-        dashOneAnimation.damping = 1.5
-        dashOneAnimation.mass = 0.22
-        dashOneAnimation.initialVelocity = 0.5
-        dashOneAnimation.timingFunction = CAMediaTimingFunction(name:kCAMediaTimingFunctionEaseInEaseOut)
-        
-        let dashTwoAnimation = CASpringAnimation(keyPath:"transform.rotation.z")
-        dashTwoAnimation.fromValue = 0.0
-        dashTwoAnimation.toValue = 45 * CGFloat(M_PI/180)
-        dashTwoAnimation.duration = 1.0
-        dashTwoAnimation.damping = 1.5
-        dashTwoAnimation.mass = 0.22
-        dashTwoAnimation.initialVelocity = 0.5
-        dashTwoAnimation.timingFunction = CAMediaTimingFunction(name:kCAMediaTimingFunctionEaseInEaseOut)
+        let dashOneAnimation = springyRotationAnimation(-45.0)
+        let dashTwoAnimation = springyRotationAnimation(45.0)
         
         dashOneLayer.transform = CATransform3DMakeRotation(-45 * CGFloat(M_PI/180), 0.0, 0.0, 1.0)
         dashTwoLayer.transform = CATransform3DMakeRotation(45 * CGFloat(M_PI/180), 0.0, 0.0, 1.0)
