2ad496ca109bacf354e23b751e62bebc1fd23d4e
Philip Kluz <philip.kluz@originate.com>
2015-09-27 20:16:01 +0200

    Enabling delayed animated content views.

--- Demo/DemoViewController.swift
+++ Demo/DemoViewController.swift
@@ -32,7 +32,12 @@ class DemoViewController: UIViewController {
     @IBAction func showAnimatedProgressHUD(sender: AnyObject) {
         PKHUD.sharedHUD.contentView = PKHUDProgressView()
         PKHUD.sharedHUD.show()
-        PKHUD.sharedHUD.hide(afterDelay: 2.0)
+        
+        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(2.0 * Double(NSEC_PER_SEC)))
+        dispatch_after(delayTime, dispatch_get_main_queue()) {
+            PKHUD.sharedHUD.contentView = PKHUDSuccessView()
+            PKHUD.sharedHUD.hide(afterDelay: 2.0)
+        }
     }
     
     @IBAction func showTextHUD(sender: AnyObject) {
